5d53eb8f359ff64a1d3195318f0b66704f9c6d53
Megan Schmi <meganyschmid@gmail.com>
2021-01-24 21:42:44 -0800

    Dialogue Text

--- Assets/VisualNovel/DialogueDisplay.cs
+++ Assets/VisualNovel/DialogueDisplay.cs
@@ -13,11 +13,39 @@ public class DialogueDisplay : MonoBehaviour
     void Start()
     {
         dialogue = gameObject.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        StartCoroutine(Print());
     }
 
     // Update is called once per frame
     void Update()
     {
-    	dialogue.SetText(text);
+        
+    }
+
+    IEnumerator Print()
+    {
+        string printedText = "";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '<')
+            {
+                while (i+1 < text.Length)
+                {
+                    if (text[i] == '>' && text[i+1] != '<')
+                    {
+                        printedText += text[i];
+                        i++;
+                        break;
+                    }
+                    printedText += text[i];
+                    i++;
+                }
+            }
+
+            printedText += text[i];
+            dialogue.text = printedText;
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
