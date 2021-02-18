2cd5ed108a09925a0f6ca23aa03c0145e486b1c5
Jaret <jaretishii17@gmail.com>
2021-02-17 18:45:06 -0800

    Puzzle Storm Spawner in Jaret Scene

---
+++ Assets/Jaret Workspace/Jaret Scripts/JaretGameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaretGameManager : MonoBehaviour
+{
+    public GameObject StormPrefab;
+
+    public Vector3[] StormList;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Vector3 i in StormList)
+        {
+            Debug.Log("Storm is Coming");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
--- Assets/Jaret Workspace/Jaret Scripts/MapManager.cs
+++ Assets/Jaret Workspace/Jaret Scripts/MapManager.cs
@@ -17,24 +17,46 @@ public class MapManager : MonoBehaviour
     
 
     private Dictionary<TileBase, TileData> dataFromTiles;
+    private List<GameObject> StormList = new List<GameObject>();
+
+
+    public GameObject StormPrefab;
+
+    public Vector3[] StormPosition;
+
+
+    private int turn;
+
+
 
     void Awake()
     {
+        turn = 0;
         dataFromTiles = new Dictionary<TileBase, TileData>();
 
-        foreach(var tileData in tileDatas)
+        foreach(var tileData in tileDatas)                 // Create and store the Tiles
         {
             foreach(var tile in tileData.tiles)
             {
                 dataFromTiles.Add(tile, tileData);
             }
         }
+
+
+        /////////////////////////////////////////////////////////// Create and Store the Storm Objects
+        
+        foreach (Vector3 i in StormPosition)
+        {
+            Vector3Int stormNum = Vector3Int.FloorToInt(i);
+            StormList.Add(Instantiate(StormPrefab, map.CellToWorld(stormNum), this.transform.rotation));
+        }
+
     }
 
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))                                // This spits out the position of a clicked on tile into console, I used it for debugging, not needed in final
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPosition = map.WorldToCell(mousePosition);
@@ -47,10 +69,23 @@ public class MapManager : MonoBehaviour
         }
     }
 
-    public Vector3 PlayerMove(Vector2 mousePosition)
+    public Vector3 PlayerMove(Vector2 mousePosition)                // Player Object calls this function
     {
         Vector3Int gridPosition = map.WorldToCell(mousePosition);
         return map.CellToWorld(gridPosition);
+        turn++;
+        if (turn >=5)
+        {
+            MoveStorms();
+        }
+    }
+
+    private void MoveStorms()
+    {
+        foreach( GameObject i in StormList)
+        {
+
+        }
     }
 
     
