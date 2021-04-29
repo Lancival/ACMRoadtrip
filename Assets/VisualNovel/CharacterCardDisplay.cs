78e7bbc1191cbfbbdf7beb72d0809b5dddaafc5c
Megan Schmid <meganyschmid@gmail.com>
2021-04-28 19:25:46 -0700

    switch btwn charas

--- Assets/VisualNovel/CharacterCardDisplay.cs
+++ Assets/VisualNovel/CharacterCardDisplay.cs
@@ -6,23 +6,19 @@ using TMPro;
 
 public class CharacterCardDisplay : MonoBehaviour
 {
-    [SerializeField] private GameObject character;
-    [SerializeField] private TextMeshProUGUI info;
-
-    // replace text with the character sprites
-
-    private List<string> texts = new List<string>
-        { "one", "two", "three" };
-
-    private int index = 0;
-    private int sizeOfList;
+    [SerializeField] private Button charaCard;
+    [SerializeField] private Button RightArrow;
+    [SerializeField] private Button LeftArrow;
+    [SerializeField] private GameObject chara1;
+    [SerializeField] private GameObject chara2;
     private bool canClick = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        info.text = texts[index];
-        sizeOfList = texts.Count;
+        chara1.SetActive(true);
+        chara2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,17 +27,29 @@ public class CharacterCardDisplay : MonoBehaviour
         if (canClick)
         {
             // the following nested code simulates how I want the screen to act, but the code is not accurate to what we want to do rn
-            if (Input.GetKey("right"))
-            {
-                StartCoroutine(TraverseRight());
-                StartCoroutine(DelayClick());
-            }
-            if (Input.GetKey("left"))
-            {
-                StartCoroutine(TraverseLeft());
-                StartCoroutine(DelayClick());
-            }
+            if (Input.GetKey("right") || Input.GetKey("left")) 
+            { Move(); }
+
+            RightArrow.onClick.AddListener(click);
+            LeftArrow.onClick.AddListener(click);
         }
+    }
+
+    void Move(){
+        if (chara1.activeSelf == true){
+            chara1.SetActive(false);
+            chara2.SetActive(true);
+        }
+        else {
+            chara1.SetActive(true);
+            chara2.SetActive(false);
+        }
+        StartCoroutine(DelayClick());
+    }
+
+    void click(){
+        Debug.Log ("You have clicked the button!");
+        Move();
     }
 
     // Delay click so you can't spam update
@@ -52,27 +60,27 @@ public class CharacterCardDisplay : MonoBehaviour
         canClick = true;
     }
 
-    IEnumerator TraverseRight()    
-    {
-        gameObject.GetComponent<Fade>().FadeOut();
-        yield return new WaitForSeconds(0.5f);
-        // traverse list rightwards
-        index++;
-        if (index > sizeOfList-1)
-            index = 0;
-        info.text = texts[index];
-        gameObject.GetComponent<Fade>().FadeIn();
-    }
+    // IEnumerator TraverseRight()    
+    // {
+    //     gameObject.GetComponent<Fade>().FadeOut();
+    //     yield return new WaitForSeconds(0.5f);
+    //     // traverse list rightwards
+    //     index++;
+    //     if (index > sizeOfList-1)
+    //         index = 0;
+    //     character = sprites[index];
+    //     gameObject.GetComponent<Fade>().FadeIn();
+    // }
 
-    IEnumerator TraverseLeft()    
-    {
-        gameObject.GetComponent<Fade>().FadeOut();
-        yield return new WaitForSeconds(0.5f);
-        // traverse list leftwards
-        index--;
-        if (index < 0)
-            index = sizeOfList-1;
-        info.text = texts[index];
-        gameObject.GetComponent<Fade>().FadeIn();
-    }
+    // IEnumerator TraverseLeft()    
+    // {
+    //     gameObject.GetComponent<Fade>().FadeOut();
+    //     yield return new WaitForSeconds(0.5f);
+    //     // traverse list leftwards
+    //     index--;
+    //     if (index < 0)
+    //         index = sizeOfList-1;
+    //     character = sprites[index];
+    //     gameObject.GetComponent<Fade>().FadeIn();
+    // }
 }
