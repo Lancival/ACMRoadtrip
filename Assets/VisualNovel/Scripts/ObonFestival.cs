15810d01f8d6747c4b957de09eaba66e1cf1c8da
Richard Cheng <rhocheng@g.ucla.edu>
2021-05-29 16:45:32 -0700

    Added debug skip puzzle key

--- Assets/VisualNovel/Scripts/ObonFestival.cs
+++ Assets/VisualNovel/Scripts/ObonFestival.cs
@@ -16,7 +16,7 @@ public class ObonFestival : MonoBehaviour
 	[SerializeField] private Image background;
 	[SerializeField] private Transform images;
 
-    private int puzzles = 0;
+    [SerializeField] private int puzzles = 0;
 
     // Start is called before the first frame update
     void Start()
